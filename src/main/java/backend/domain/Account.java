87350003e6d471f9e615e7f2afcb9d02a9cb2df4
조흥재 연구원 <heung@ubisam.com>
2026-04-23 10:29:23 +0900

    20260423 도메인 생성

---
+++ src/main/java/backend/domain/Account.java
@@ -0,0 +1,38 @@
+package backend.domain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import backend.domain.auditing.AuditedEntity;
+import backend.domain.properties.AttributesSet;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@Table(name = "example_account")
+@EqualsAndHashCode(callSuper = true)
+public class Account extends AuditedEntity{
+
+  @Id
+  private String id;
+
+  private String provider;
+  private String username;
+  private AttributesSet roles;
+  
+  public static Collection<GrantedAuthority> getAuthorities(AttributesSet roles){
+    if(roles == null){
+      return Collections.emptyList();
+    }
+    return roles.stream().map(r -> new SimpleGrantedAuthority(r.toString())).collect(Collectors.toList());
+  }
+  
+}
